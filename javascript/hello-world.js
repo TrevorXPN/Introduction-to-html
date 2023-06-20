b5b7a76cdcf8cf02cb2f2bb0abec8425175f3c95
Trevor Kayongo <trevorssetuba@gmail.com>
2023-06-20 15:23:43 +0300

    First Java script class

---
+++ javascript/hello-world.js
@@ -0,0 +1,68 @@
+console.log("Hey Trevor")
+
+// 1.Comment
+// Used to provide information about the code
+// Can be used in general to provide any info or suggestion
+// Makes it easy to understand the code or 
+// Used to comment out code 
+// comments are ignored by javascript engin
+// can be multi or single line
+// EXAMPLES
+// This is a single-line comment
+/*
+this is a multi-line comment
+*/
+
+// 2.Variables
+// Used to store data
+// Can be looked at as containers for storing data values
+// must contain only letters , digits, or $ and _ symbole
+
+// GUIDELINES FOR NAMING Variables
+// Must begin with 
+// Must not begin with a number
+// Must not be a reserved Key word like if, else , boolean ,let ,var ,const etc
+// Are case sensitive
+// Use meaningful names
+// Use camelcasing
+
+// var was used in earlier versions but now we use let and const
+// EXAMPLES
+// Variable declaration
+let age;
+// variable initialization
+age = 12;
+/*
+*/
+
+let studentName = "Trevor";
+/*
+"" are called strings
+*/
+console.log(studentName)
+
+// declaring may variebles on the same line
+let firstName, lastName, cohort, year;
+firstName = "TREVOR";
+lastName = "KAYONGO";
+cohort = 10;
+year = 2023;
+
+console.log(lastName)
+
+// dealing with constants
+const carName = "Benz";
+console.log(carName)
+ 
+// carName = "lux"
+// console.log(carName)
+
+//NAMING CONVENTIONS
+// camelcase eg. firstNAme
+// pascalcase eg FirstName
+// snakecase eg firstname
+// all caps snakecase eg FIRSTNAME
+// kebabcase eg first-name
+
+// NB;
+// VARIABLES ARE CASE SENSITIVE
